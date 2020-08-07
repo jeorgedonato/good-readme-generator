cdb70ef23d394437b16075558c4848803b83fa5a
Jeorge Donato <jeorgekhenr@gmail.com>
2020-08-06 21:37:11 -0700

    Added questions

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const inquirer = require('inquirer');
 const generateMark = require('./utils/generateMarkdown.js')
 
 const questions = [
-  inquirer.prompt([
-
-  ]).then(answers => {
-
-  }).catch(error => {
-
-  })
+  {
+    type: 'input',
+    message: 'What is your Project Title?',
+    name: 'title'
+  },
+  {
+    type: 'editor',
+    message: 'What is your Project Description? (Close the editor after saving)',
+    name: 'description'
+  },
+  {
+    type: 'confirm',
+    message: 'Do you want the table of contents to be generated?',
+    name: 'tableOfContents'
+  },
+  {
+    type: 'editor',
+    message: 'What is your Project Description? (Close the editor after saving)',
+    name: 'description'
+  },
+  {
+    type: ''
+  }
 ];
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
+  inquirer.prompt(questions).then(answers => {
+    console.log(answers)
+  }).catch(error => {
 
+  })
 }
 
 init();
